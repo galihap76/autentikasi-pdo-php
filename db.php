f3f3c7cae3c711cee3f3d5aca79dd1542d4a1a01
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-05-07 22:49:48 +0700

    commit

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$servername;dbname=db_auth_pdo", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+function tutupKoneksi($stmt = null, $conn = null)
+{
+    // cek apakah parameter statement dan koneksi tidak null
+    if ($stmt !== null && $conn !== null) {
+        // tutup statement dan koneksi database
+        $stmt->closeCursor();
+        $conn = null;
+    }
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login']) && !isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+} else {
+
+    session_regenerate_id();
+    $identify_username = $_SESSION['identify'];
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+
+    Nama pengguna Anda adalah : <?php echo $identify_username; ?>
+    <br>
+    <br>
+
+    <a href="logout.php">Log out</a>
+
+    <?php
+
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+
+        echo "<script>
+    Swal.fire(
+        'BERHASIL!',
+        'Selamat datang $username.',
+        'success'
+    )
+    </script>";
+
+        // hapus session success registrasi
+        unset($_SESSION['username']);
+
+        // cek jika password berhasil di ubah
+    }
+
+    ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
