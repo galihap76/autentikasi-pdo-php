d4f58eb0c58a94b991c9adf6be7eb3898890690f
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-07-31 13:07:19 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,7 @@
 <?php
-
-// Koneksi ke database
 $servername = "localhost";
 $username = "root";
 $password = "";
 
 $conn = new PDO("mysql:host=$servername;dbname=db_auth_pdo", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// Tutup koneksi database
-function tutupKoneksi($conn)
-{
-    $conn = null;
-}
